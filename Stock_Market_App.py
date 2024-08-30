a46ef5140e721463e4ebc20bab81902e6379a55c
A-vidhyalakshmi <147320120+A-vidhyalakshmi@users.noreply.github.com>
2024-08-30 18:02:29 +0530

    with columns
    start and end data as columns

--- Stock_Market_App.py
+++ Stock_Market_App.py
@@ -3,8 +3,14 @@ import yfinance as yf
 import datetime
 
 ticker_symbol = st.text_input("Enter the stock name:", "AAPL")
-start_date = st.date_input("START DATE", value=datetime.date(2019,1,7))
-end_date = st.date_input("END DATE", value=datetime.date(2023,1,7))
+
+col1, col2 = st.columns(2)
+
+with col1:
+    start_date = st.date_input("START DATE", value=datetime.date(2019,1,7))
+
+with col2:
+    end_date = st.date_input("END DATE", value=datetime.date(2023,1,7))
 
 
 data = yf.download(ticker_symbol, start = start_date, end=end_date)
